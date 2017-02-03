87d8a976fbba45494eaa677af379a8228a95bdef
Alessio Roberto <darthpelo@gmail.com>
2017-02-03 23:59:25 +0100

    🔧 Improved value check in yellow and green functions

--- RPiLedsControl/Sources/App/main.swift
+++ RPiLedsControl/Sources/App/main.swift
@@ -14,48 +14,48 @@ let gpioLib = GPIOLib.sharedInstance
 let list: [GPIOName] = [.P20, .P26]
 let ports = gpioLib.setupOUT(ports: list, for: .RaspberryPi2)
 
-func yellow() {
-  if (ports[.P20]?.value == 0) {
-      gpioLib.switchOn(ports: [.P20])
-  } else {
-      gpioLib.switchOff(ports: [.P20])
-  }
-}
-
-func green() {
-  if (ports[.P26]?.value == 0) {
-      gpioLib.switchOn(ports: [.P26])
-  } else {
-     gpioLib.switchOff(ports: [.P26])
-  }
-}
-
-func powerOff() {
-  gpioLib.switchOff(ports: list)
-}
-
 func status(_ port: GPIO?) -> Int {
     guard let port = port else {
         return 0
     }
-
+    
     return port.value
+}
+
+func yellow() {
+    if (status(ports[.P20]) == 0) {
+        gpioLib.switchOn(ports: [.P20])
+    } else {
+        gpioLib.switchOff(ports: [.P20])
+    }
+}
+
+func green() {
+    if (status(ports[.P26]) == 0) {
+        gpioLib.switchOn(ports: [.P26])
+    } else {
+        gpioLib.switchOff(ports: [.P26])
+    }
+}
+
+func powerOff() {
+    gpioLib.switchOff(ports: list)
 }
 
 let drop = Droplet()
 
 drop.get { req in
     return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+        "message": drop.localization[req.lang, "welcome", "title"]
+        ])
 }
 
 drop.get("cmd", ":id") { request in
-  guard let cmdId = request.parameters["id"]?.int else {
-    throw Abort.badRequest
-  }
-  
-  switch(cmdId) {
+    guard let cmdId = request.parameters["id"]?.int else {
+        throw Abort.badRequest
+    }
+    
+    switch(cmdId) {
     case Command.Zero.rawValue:
         powerOff()
     case Command.One.rawValue:
@@ -64,14 +64,14 @@ drop.get("cmd", ":id") { request in
         green()
     default:
         throw Abort.badRequest
-  }
-  //return "You execute command #\(cmdId)"
-  return try JSON(node: [
-        "version": "0.1",
+    }
+    
+    return try JSON(node: [
+        "version": "1.0.0",
         "command": "\(cmdId)",
         "yellow": "\(status(ports[.P20]))",
         "green": "\(status(ports[.P26]))"
-    ])
+        ])
 }
 
 drop.resource("posts", PostController())
